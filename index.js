2ea8ddf64745b3c24c324d51fc00465882f74efe
ROHAN <ro.anilpnr@gmail.com>
2021-04-29 12:46:56 +0530

    time-fix

---
+++ index.js
@@ -0,0 +1,51 @@
+currentDate = new Date();
+
+
+
+periodTimings = {
+    1:[[8,30],[9,10]],
+    2:[[9,20],[10,10]],
+    3:[[10,10],[10,50]],
+    4:[[11,00],[11,40]],
+    5:[[11,50],[12,30]],
+    6:[[12,40],[13,20]],
+    7:[[13,30],[14,10]],
+}
+
+function getPeriodNumber(){
+    for(let period in periodTimings){
+        startDate = new Date(currentDate.getTime());
+        startDate.setHours(periodTimings[period][0][0]);
+        startDate.setMinutes(periodTimings[period][0][1]);
+        
+
+        endDate = new Date(currentDate.getTime());
+        endDate.setHours(periodTimings[period][1][0]);
+        endDate.setMinutes(periodTimings[period][1][1]);
+
+        if(startDate < currentDate && endDate > currentDate){
+            console.log(period);
+            return period;
+        }
+        
+    }
+}
+
+function periodSelector(row, column){
+    const _row = document.querySelector('.time-table').rows[row];
+    periods=[];
+    
+    for(let i=0; col=_row.cells[i]; i++){
+        if(col.colSpan==2){
+            periods.push(col);
+            periods.push(col);
+        }
+        else{
+            periods.push(col);
+        }
+    }
+    return periods;
+}
+
+const currentCell = periodSelector(currentDate.getDay())[getPeriodNumber()];
+currentCell.style.backgroundColor='#a9ffa6';
